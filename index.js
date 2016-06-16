83f0eadc36ad6eb33b82bc0acf386f6019b53592
Johnny Gigla <giglajo@katharineum.de>
2016-06-16 17:20:23 +0200

    Add some comments to endpoints

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ var config = require('./config.json');
 
 app.use(bodyParser.json());
 
+// Test request
+// No parameters
+// Always returns "test"
 app.get('/', (req, res) => {
   res.send('test')
 });
 
+// Create an auth token
+// Parameters (JSON):
+//   key - the admin key defined on the server
+//   userId - the user ID to create a token for
+//   expireSec - after how many seconds this token should expire
+// Returns (JSON):
+//   token - the created token
 app.post('/auth', (req, res) => {
   if (req.body.key === config.adminKey) {
     datastore.Auths.create(req.body.userId, req.body.expireSec).then(token => {
@@ -22,6 +32,14 @@ app.post('/auth', (req, res) => {
   }
 });
 
+// Create a user
+// Parameters (JSON):
+//   key - the admin key defined on the server
+//   name - a name for the user, doesn't have to be unique
+//   lat - the user's latitude
+//   lon - the user's longitude
+// Returns (JSON):
+//   id - the created user's ID
 app.post('/users', (req, res) => {
   if (req.body.key === config.adminKey) {
     datastore.Users.create(req.body.name, req.body.lat, req.body.lon).then(id => {
@@ -32,6 +50,13 @@ app.post('/users', (req, res) => {
   }
 });
 
+// Get a user by ID
+// Parameters (URL):
+//   id - the ID for which the user should be obtained
+// Returns (JSON):
+//   name - the obtained user's name
+//   lat - the user's latitude
+//   lon - the user's longitude
 app.get('/users/:id', (req, res) => {
   datastore.Users.get(req.params.id).then(user => {
     if (user) {
@@ -42,6 +67,16 @@ app.get('/users/:id', (req, res) => {
   });
 });
 
+// Create an entry
+// Parameters (JSON):
+//   userId - the ID of the creating user
+//   token - an auth token valid for the creating user
+//   time - the unix timestamp at which this entry should be created
+//   type - the type of entry to be created (arbitrary integer, to be defined
+//          later)
+//   value - the value this entry should have, in a unit defined by the type
+// Returns (JSON):
+//   id - the ID of the created entry
 app.post('/entries', (req, res) => {
   datastore.Auths.isValid(req.body.userId, req.body.token).then(valid => {
     if (valid) {
@@ -54,24 +89,47 @@ app.post('/entries', (req, res) => {
   })
 });
 
+// Get all entries of a particular type
+// Parameters (URL):
+//   type - the type to get entries for (arbitrary integer)
+// Returns (JSON) - an array of entry IDs
 app.get('/entries/type/:type', (req, res) => {
   datastore.Entries.allOfType(req.params.type).then(entries => {
     res.json(entries);
   })
 });
 
+// Get all entries of a particular user
+// Parameters (URL):
+//   userId - the user ID to get entries for
+// Returns (JSON) - an array of entry IDs
 app.get('/entries/user/:userId', (req, res) => {
   datastore.Entries.allForUser(req.params.userId).then(entries => {
     res.json(entries);
   })
 });
 
+// Get all entries of a particular user and type
+// Parameters (URL):
+//   userId - the user ID to get entries for
+//   type - the type to get entries for (arbitrary integer)
+// Returns (JSON) - an array of entry IDs
 app.get('/entries/all/:userId/:type', (req, res) => {
   datastore.Entries.all(req.params.userId, req.params.type).then(entries => {
     res.json(entries);
   });
 });
 
+// Get the most recent entry (highest time) of a particular user and type
+// Parameters (URL):
+//   userId - the user ID to get an entry for
+//   type - the type to get an entry for
+// Returns (JSON):
+//   id - the ID of the entry
+//   userId - the ID of the user that created the entry
+//   time - the unix timestamp at which the entry was created
+//   type - the type the entry is (arbitrary integer, to be defined later)
+//   value - the value the entry has, in a unit defined by the type
 app.get('/entries/most-recent/:userId/:type', (req, res) => {
   datastore.Entries.mostRecent(req.params.userId, req.params.type).then(id => {
     datastore.Entries.get(id).then(entry => {
@@ -81,6 +139,14 @@ app.get('/entries/most-recent/:userId/:type', (req, res) => {
   });
 });
 
+// Get a particular entry by ID
+// Parameters (URL):
+//   id - the ID for which to get the entry
+// Returns (JSON):
+//   userId - the ID of the user that created the entry
+//   time - the unix timestamp at which the entry was created
+//   type - the type the entry is (arbitrary integer, to be defined later)
+//   value - the value the entry has, in a unit defined by the type
 app.get('/entries/:id', (req, res) => {
   datastore.Entries.get(req.params.id).then(entry => {
     if (entry) {
